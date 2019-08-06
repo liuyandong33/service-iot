cccbfd39e10f50c0143e7c00e9f7b4ba6fe9c444
liuyandong <554030404@qq.com>
2019-08-06 22:50:21 +0800

    初始化项目！！

--- src/main/java/build/dream/iot/constants/Constants.java
+++ src/main/java/build/dream/iot/constants/Constants.java
@@ -4,6 +4,4 @@ package build.dream.iot.constants;
  * Created by liuyandong on 2017/5/5.
  */
 public class Constants extends build.dream.common.constants.Constants {
-    public static final String RESOURCE_ID_IOT = "iot";
-    public static final String PONG = "PONG";
 }
